1c01ae4832160e55b0a6b5813e2cf505f9c56f02
Bharat Agrawal <freelancer.bharatagrawal@gmail.com>
2026-02-05 00:46:04 +0530

    Add files via upload

--- middleware/cors.js
+++ middleware/cors.js
@@ -1,11 +1,23 @@
 import cors from "cors"
 import dotenv from "dotenv";
 dotenv.config();
-export const corsConfig = cors({
-  origin: process.env.FRONTEND_URL,
-  credentials: true,
-  methods: ["GET", "POST", "PUT", "DELETE"],
+const allowedOrigins = [
+  "https://acadhelp.in",
+  "https://www.acadhelp.in",
+];
 
-})
+export const corsConfig = cors({
+  origin: function (origin, callback) {
+    if (!origin) return callback(null, true);
+
+    if (allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error("Not allowed by CORS"));
+    }
+  },
+  credentials: true,
+});
+
 
 export default corsConfig
